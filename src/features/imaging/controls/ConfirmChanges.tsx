a54fc12d1163f2bd531737d6642410ce7903af14
kirkwood616 <kirkwood616@gmail.com>
2025-07-03 14:20:28 -0400

    separated edit & create, added handleCreate functions

--- src/features/imaging/controls/ConfirmChanges.tsx
+++ src/features/imaging/controls/ConfirmChanges.tsx
@@ -1,22 +1,29 @@
 import Loading from "@/components/loading/Loading";
 import GoButton from "@/components/ui/buttons/GoButton";
 import {
+  addNewOrderRecord,
   updateOrderRecord,
   updateRecordLock,
 } from "@/services/imagingServices";
 import { useImagingDraftStore } from "@/stores/imaging/useImagingDraftStore";
 import { useImagingOrderStore } from "@/stores/imaging/useImagingOrderStore";
 import { useLoadingStore } from "@/stores/loading/useLoadingStore";
+import type { ImagingRecord } from "@/types/Imaging";
 import { useNavigate } from "react-router-dom";
 import styles from "./ConfirmChanges.module.css";
 
-function ConfirmChanges() {
-  const { order } = useImagingOrderStore();
+interface Props {
+  isEdit?: boolean;
+  isCreate?: boolean;
+}
+
+function ConfirmChanges({ isEdit, isCreate }: Props) {
+  const { order, setOrder } = useImagingOrderStore();
   const { draftRecord, resetDraft } = useImagingDraftStore();
   const { isLoading, setIsLoading } = useLoadingStore();
   const navigate = useNavigate();
 
-  async function handleCancel() {
+  async function handleEditCancel() {
     setIsLoading(true);
     try {
       await updateRecordLock(order.id!, false);
@@ -29,7 +36,7 @@ function ConfirmChanges() {
     }
   }
 
-  async function handleSave() {
+  async function handleEditSave() {
     setIsLoading(true);
     const updatedRecord = { ...draftRecord };
     delete updatedRecord.id;
@@ -44,17 +51,59 @@ function ConfirmChanges() {
     }
   }
 
+  async function handleCreateCancel() {
+    setIsLoading(true);
+    try {
+      resetDraft();
+      navigate(-1);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleCreateSave() {
+    setIsLoading(true);
+    const newRecord = { ...draftRecord };
+    delete newRecord.id;
+    try {
+      const addedDoc = await addNewOrderRecord(newRecord);
+      resetDraft();
+      const newDoc: ImagingRecord = { id: String(addedDoc.id), ...newRecord };
+      setOrder(newDoc);
+      navigate(`/imaging/${newDoc.status}/${newDoc.id}`);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   if (isLoading) return <Loading />;
-  return (
-    <div className={styles.ConfirmChanges}>
-      <GoButton type="cancel" clickFunction={handleCancel}>
-        CANCEL
-      </GoButton>
-      <GoButton type="primary" clickFunction={handleSave}>
-        SAVE
-      </GoButton>
-    </div>
-  );
+  if (isEdit)
+    return (
+      <div className={styles.ConfirmChanges}>
+        <GoButton type="cancel" clickFunction={handleEditCancel}>
+          CANCEL
+        </GoButton>
+        <GoButton type="primary" clickFunction={handleEditSave}>
+          SAVE
+        </GoButton>
+      </div>
+    );
+
+  if (isCreate)
+    return (
+      <div className={styles.ConfirmChanges}>
+        <GoButton type="cancel" clickFunction={handleCreateCancel}>
+          CANCEL
+        </GoButton>
+        <GoButton type="primary" clickFunction={handleCreateSave}>
+          SAVE
+        </GoButton>
+      </div>
+    );
 }
 
 export default ConfirmChanges;
