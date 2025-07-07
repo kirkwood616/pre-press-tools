fe03299e2b8c77b330e93e2c99640aead672801e
kirkwood616 <kirkwood616@gmail.com>
2025-07-07 00:10:21 -0400

    removed container + id prop, added order store

--- src/features/imaging/controls/EditOrder.tsx
+++ src/features/imaging/controls/EditOrder.tsx
@@ -1,21 +1,18 @@
 import GoButton from "@/components/ui/buttons/GoButton";
 import EditIcon from "@/icons/EditIcon";
+import { useImagingOrderStore } from "@/stores/imaging/useImagingOrderStore";
 import { useLoadingStore } from "@/stores/loading/useLoadingStore";
 import { useNavigate } from "react-router-dom";
-import styles from "./EditOrder.module.css";
 
-interface Props {
-  recordID: string;
-}
-
-function EditOrder({ recordID }: Props) {
+function EditOrder() {
+  const { order } = useImagingOrderStore();
   const { setIsLoading } = useLoadingStore();
   const navigate = useNavigate();
 
   async function handleEditOrder() {
     setIsLoading(true);
     try {
-      navigate(`/imaging/edit/${recordID}`);
+      navigate(`/imaging/edit/${order.id!}`);
     } catch (error) {
       console.error(error);
     } finally {
@@ -24,12 +21,10 @@ function EditOrder({ recordID }: Props) {
   }
 
   return (
-    <div className={styles.EditOrder}>
-      <GoButton type={"secondary"} clickFunction={handleEditOrder}>
-        <EditIcon />
-        EDIT ORDER
-      </GoButton>
-    </div>
+    <GoButton type={"secondary"} clickFunction={handleEditOrder}>
+      <EditIcon />
+      EDIT ORDER
+    </GoButton>
   );
 }
 
